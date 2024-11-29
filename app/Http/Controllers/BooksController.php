396bbe7d7f7c81814d7567f4b082c067cf7c8624
T7Walker <juandamuoz21@gmail.com>
2024-11-29 15:17:43 -0500

    Version1.10.1
    Controladores de prueba CRUD

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -2,9 +2,61 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Usuario;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
 
 class BooksController
 {
-    //
+    public function index()
+    {
+
+        return view("Books.pushBooks");
+    }
+
+    public function store(Request $rqs)
+    {
+
+        $tiltle = $rqs->input("tiltle");
+        $url = $rqs->input("url");
+
+        Usuario::create([
+
+
+            "tiltle" => $rqs->input("tiltle"),
+            "url" => $rqs->input("url"),
+            'date_creation' => Carbon::now()
+
+
+        ]);
+
+        return redirect()->route('Books.allBooks')->with('success', 'pushBookSucess');
+    }
+
+    public function show($id)
+    {
+
+        //aqui va el show
+
+    }
+    public function edit($id)
+    {
+
+        //aqui va el edit
+
+    }
+
+    public function update($id)
+    {
+
+        //aqui va la update
+
+    }
+    public function destroy($id)
+    {
+
+        //aqui va el destroy
+
+    }
 }
--- app/Http/Controllers/Profile.php
+++ app/Http/Controllers/Profile.php
@@ -8,43 +8,43 @@ use Illuminate\Support\Facades\Auth;
 
 class Profile
 {
-	// Esta funcion actualiza valores de la base de datos del usuario.
+    // Esta funcion actualiza valores de la base de datos del usuario.
     public function update(Request $rqs)
     {
-		// Nuevamente tomamos al usuario registrado.
-		$user = Auth::user();
-		
-		// Aqui se esta tomando los valores del usuario, si revisas el HTML de la pagina principal, los inputs de name y email, tienen un valor llamado name="", ese es el input que recibe Request. En estas variables esta pues obviamente lo que se puso en el input.
+        // Nuevamente tomamos al usuario registrado.
+        $user = Auth::user();
+
+        // Aqui se esta tomando los valores del usuario, si revisas el HTML de la pagina principal, los inputs de name y email, tienen un valor llamado name="", ese es el input que recibe Request. En estas variables esta pues obviamente lo que se puso en el input.
         $name = $rqs->input('name');
         $email = $rqs->input('email');
-		
-		// Aqui se le indica a la base de datos que actualice los valores ya existentes del usuario PERO, solo valores independientes, no todos los valotes. Como el nombre y email.
+
+        // Aqui se le indica a la base de datos que actualice los valores ya existentes del usuario PERO, solo valores independientes, no todos los valotes. Como el nombre y email.
         $user->name = $name;
         $user->email = $email;
-		
-		// Se guardan los cambios
-		$user->save();
-		
-		// Se redirecciona a la pagina principal con un mensaje al HTML indicando el cambio.
+
+        // Se guardan los cambios
+        $user->save();
+
+        // Se redirecciona a la pagina principal con un mensaje al HTML indicando el cambio.
         return redirect()->route('mainPage.index')->with('session_type', 'profileChanged');
     }
-	
-	// Este metodo es para eliminar a los usuarios de la base de datos y cerrarles la sesion.
+
+    // Este metodo es para eliminar a los usuarios de la base de datos y cerrarles la sesion.
     public function deleted(Request $rqs)
     {
-		// Se toma el ID del usuario registrado primero que nada y luego se busca todos sus datos en la base de datos de usuarios.
+        // Se toma el ID del usuario registrado primero que nada y luego se busca todos sus datos en la base de datos de usuarios.
         $user = Usuario::find(Auth::user()->id);
 
-		// Se procede a eliminalo porque nadie lo quiere.
+        // Se procede a eliminalo porque nadie lo quiere.
         $user->delete();
 
-		// Aqui se aplica la misma logica de logout, revisa el codigo del controlador login.
+        // Aqui se aplica la misma logica de logout, revisa el codigo del controlador login.
         $rqs->session()->invalidate();
         $rqs->session()->regenerateToken();
 
         Auth::logout();
 
-		// Se redirecciona al register de la misma forma que pasa al hacer logout, revisa el codigo del controlador login.
+        // Se redirecciona al register de la misma forma que pasa al hacer logout, revisa el codigo del controlador login.
         return redirect()->route('register.index')->with('session_type', 'profileDeleted');
     }
 }
