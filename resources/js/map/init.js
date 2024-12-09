a8f11fa168c774ee725d610f983ac3a569732fb1
T7Walker <juandamuoz21@gmail.com>
2024-12-09 18:31:59 -0500

    Version1.11.2
    Cambios a la configuracion deñ mapa

--- resources/js/map/init.js
+++ resources/js/map/init.js
@@ -21,14 +21,53 @@ new mapboxgl.Marker().setLngLat([-76.6092, 2.4448]).setPopup(new mapboxgl.Popup(
 
 
 // Aqui estan los botones
+
+//RUTAS TRANSPUBENZA
+
 const TP1BTBtn = document.getElementById('TP1BTroute')
 const TP2BTBtn = document.getElementById('TP2BTroute')
 const TP2MBtn = document.getElementById('TP2Mroute')
 const TP4BTBtn = document.getElementById('TP4BTroute')
+const TP5BTBtn = document.getElementById('TP5BTroute')
+const TP5MBtn = document.getElementById('TP5Mroute')
+const TP6BTBtn = document.getElementById('TP6BTroute')
+const TP7BTBtn = document.getElementById('TP7BTroute')
+const TP7MBtn = document.getElementById('TP7Mroute')
+const TP8BTBtn = document.getElementById('TP9BTroute')
+const TP8MBtn = document.getElementById('TP8Mroute')
+const TP9MBtn = document.getElementById('TP9Mroute')
+const TP10MBtn = document.getElementById('TP10Mroute')
+const TP11MBtn = document.getElementById('TP11Mroute')
+
+//RUTAS TRANSTAMBO
+
+const TT1MBtn = document.getElementById('TT1Mroute')
+const TT2Mbtm = document.getElementById('TT2Mroute')
+const TT3MBtn = document.getElementById('TT3Mroute')
+const TT4MBtn = document.getElementById('TT4Mroute')
+const TT5MBtn = document.getElementById('TT5Mroute')
+const TT6MBtn = document.getElementById('TT6Mroute')
+
+//RUTAS SOTRACAUCA
+
+const SC1MBtn = document.getElementById('SC1Mroute')
+const SC2MBtn = document.getElementById('SC2Mroute')
+const SC3MBtn = document.getElementById('SC3Mroute')
+const SC4MBtn = document.getElementById('SC4Mroute')
+const SC5MBtn = document.getElementById('SC5Mroute')
+const SC6MBtn = document.getElementById('SC6Mroute')
+const SC7MBtn = document.getElementById('SC7Mroute')
+const SC8MBtn = document.getElementById('SC8Mroute')
+const SC9MBtn = document.getElementById('SC9Mroute')
+
 
 
 // Eventos 'click' de los botones
 // En caso de añadirse mas botones o mas datos, edita routes.dataTranspubenza por el nombre que le pusiste a la variable, por ejemplo, aqui es dataTranspubenza, entonces si el tuyo es dataTranspubenza2, seria routes.dataTranspubenza2.
+
+
+//RUTAS TRANSPUBENZA
+
 TP1BTBtn.addEventListener('click', () => {
     const { id, coordinates, waypoints = [], color } = routes.dataTranspubenzaTP1BT;
 
@@ -52,6 +91,211 @@ TP4BTBtn.addEventListener('click', () => {
     const { id, coordinates, waypoints = [], color } = routes.dataTranspubenzaTP4BT;
 
     addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+});
+
+TP5BTBtn.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataTranspubenzaTP5BT;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
+});
+
+TP5MBtn.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataTranspubenzaTP5M;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
+});
+
+TP6BTBtn.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataTranspubenzaTP6BT;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
+});
+
+TP7BTBtn.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataTranspubenzaTP7BT;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
+});
+
+
+TP7MBtn.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataTranspubenzaTP7M;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
+});
+
+TP8BTBtn.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataTranspubenzaTP8BT;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
+});
+
+TP8MBtn.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataTranspubenzaTP8M;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
+});
+
+TP9MBtn.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataTranspubenzaTP9M;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
+});
+
+TP10MBtn.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataTranspubenzaTP10M;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
+});
+
+TP11MBtn.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataTranspubenzaTP11M;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
+});
+
+//RUTAS TRANSTAMBO
+
+TT1MBtn.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataTrastamboTT1M;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
+});
+
+TT2Mbtm.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataTrastamboTT2M;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
+});
+
+TT3MBtn.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataTrastamboTT3M;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
+});
+
+TT4MBtn.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataTrastamboTT4M;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
+});
+
+TT5MBtn.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataTrastamboTT5M;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
+});
+
+TT6MBtn.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataTrastamboTT6M;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
+});
+
+//RUTAS SOTRACAUCA
+
+SC1MBtn.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataSotracaucaSC1M;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
+});
+
+SC2MBtn.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataSotracaucaSC2M;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
+});
+
+SC3MBtn.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataSotracaucaSC3M;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
+});
+
+SC4MBtn.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataSotracaucaSC4M;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
+});
+
+SC5MBtn.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataSotracaucaSC5M;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
+});
+
+SC6MBtn.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataSotracaucaSC6M;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
+});
+
+SC7MBtn.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataSotracaucaSC7M;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
+});
+
+SC8MBtn.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataSotracaucaSC8M;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
+});
+
+SC9MBtn.addEventListener('click', () => {
+
+    const { id, coordinates, waypoints = [], color } = routes.dataSotracaucaSC9M;
+
+    addAdjustedRoute(id, color, coordinates[0], waypoints, coordinates[1]);
+
 });
 
 
--- resources/views/map/index.blade.php
+++ resources/views/map/index.blade.php
@@ -24,6 +24,13 @@
     <nav>
         @include('principal.navbar')
     </nav>
+
+    <h1 class="colorTitulo">Mapas</h1>
+
+    <br>
+    <br>
+    <br>
+
     <main>
         <div class="contendor">
             <section class="map-container">
@@ -31,22 +38,58 @@
             </section>
         </div>
         <br><br>
+        <br>
+        <br>
+        <br>
+        <!-- Organización de botones en columnas -->
         <div>
             <nav class="contendor">
-                <section class="map-controls">
-                    <button id="TP1BTroute">RutaTP1BT Transpubenza</button>
-                    <button id=""></button>
+                <section class="map-controls blue-column" name="transpubenza-Routes">
+                    <h3>Transpubenza</h3>
+                    <button id="TP1BTroute">Ruta TP1BT</button>
+                    <button id="TP2BTroute">Ruta TP2BT</button>
+                    <button id="TP2Mroute">Ruta TP2M</button>
+                    <button id="TP4BTroute">Ruta TP4BT</button>
+                    <button id="TP5BTroute">Ruta TP5BT</button>
+                    <button id="TP5Mroute">Ruta TP5M</button>
+                    <button id="TP6BTroute">Ruta TP6BT</button>
+                    <button id="TP7BTroute">Ruta TP7BT</button>
+                    <button id="TP7Mroute">Ruta TP7M</button>
+                    <button id="TP9BTroute">Ruta TP9BT</button>
+                    <button id="TP8Mroute">Ruta TP8M</button>
+                    <button id="TP9Mroute">Ruta TP9M</button>
+                    <button id="TP10Mroute">Ruta TP10M</button>
+                    <button id="TP11Mroute">Ruta TP11M</button>
+                </section>
+
+                <section class="map-controls yellow-column" name="Transtambo">
+                    <h3>Transtambo</h3>
+                    <button id="TT1Mroute">Ruta TT1M</button>
+                    <button id="TT2Mroute">Ruta TT2M</button>
+                    <button id="TT3Mroute">Ruta TT3M</button>
+                    <button id="TT4Mroute">Ruta TT4M</button>
+                    <button id="TT5Mroute">Ruta TT5M</button>
+                    <button id="TT6Mroute">Ruta TT6M</button>
+                </section>
+
+                <section class="map-controls red-column" name="Sotracauca">
+                    <h3>Sotracauca</h3>
+                    <button id="SC1Mroute">Ruta SC1M</button>
+                    <button id="SC2Mroute">Ruta SC2M</button>
+                    <button id="SC3Mroute">Ruta SC3M</button>
+                    <button id="SC4Mroute">Ruta SC4M</button>
+                    <button id="SC5Mroute">Ruta SC5M</button>
+                    <button id="SC6Mroute">Ruta SC6M</button>
+                    <button id="SC7Mroute">Ruta SC7M</button>
+                    <button id="SC8Mroute">Ruta SC8M</button>
+                    <button id="SC9Mroute">Ruta SC9M</button>
                 </section>
             </nav>
         </div>
     </main>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+    <br><br><br><br><br><br>
     @include('principal.footer')
 </body>
+
 
 </html>
